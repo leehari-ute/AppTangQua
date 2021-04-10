a4dbe34c1fc8e9c8be85daa4548c6d96623f6740
peunruly <quynhdothy@gmail.com>
2021-04-10 08:15:55 +0700

    [Quynh: Login form]

--- Android_Project_CNPM/app/src/main/java/com/example/app_quatang/Login_form.java
+++ Android_Project_CNPM/app/src/main/java/com/example/app_quatang/Login_form.java
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Login_form extends AppCompatActivity {
+
+    TextView txtviewDKN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
         getSupportActionBar().setTitle("Login");
+        Init();
+
+        txtviewDKN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Signup_form.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
-    public void btn_signup(View view) {
-        startActivity(new Intent(getApplicationContext(),Signup_form.class)) ;
+    private void Init(){
+        txtviewDKN = findViewById(R.id.textViewDKN);
     }
 }
